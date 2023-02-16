cec26de5f6435b0e9320cb1499e1ca3cf69b64c1
karthikvch <109299135+karthikvch@users.noreply.github.com>
2023-02-16 23:28:07 +0530

    Update test_release.sh

--- test_release.sh
+++ test_release.sh
@@ -2,7 +2,7 @@
 local_path=$1
 echo local_path is $local_path
 ###############################monarch-devops#############################
-chmod -R 775 scripts/*.sh
+chmod -R 775  *.sh
 pwd
 cd ..
 
