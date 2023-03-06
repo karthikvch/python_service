62d93078b41a54eaa66dad7ba2624264bac4f05d
kchitrothu <kchitrothu@monarchtractor.com>
2023-03-07 02:06:44 +0530

    added

--- scripts/pipeline/release_build.sh
+++ scripts/pipeline/release_build.sh
@@ -4,6 +4,22 @@ token=$1
 api_token=$2
 appversion=$(cat version-release.json|grep unified_version|cut -d ":" -f2)
 echo $appversion
+
+
+unified_version=`jq '.unified_version' version-release.json`
+echo $unified_version
+
+
+
+name=`jq '.applications[0].name' version-release.json`
+echo $name
+
+
+tag=`jq '.applications[0].tag' version-release.json`
+echo $tag
+
+
+
 pwd
 cd /tmp
 pwd
